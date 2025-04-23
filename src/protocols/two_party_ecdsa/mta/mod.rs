559ca03369f63ea5bbdfd4763f244bf3eef63d9f
none-31d <castrod152@swu.ac.kr>
2025-04-23 16:13:33 +0900

    new branch

--- src/protocols/two_party_ecdsa/mta/mod.rs
+++ src/protocols/two_party_ecdsa/mta/mod.rs
@@ -14,7 +14,7 @@
     @license GPL-3.0+ <https://github.com/KZen-networks/multi-party-ecdsa/blob/master/LICENSE>
 */
 
-const SECURITY_BITS: usize = 128;
+const SECURITY_BITS: usize = 256;
 
 pub mod party_one;
 pub mod party_two;
